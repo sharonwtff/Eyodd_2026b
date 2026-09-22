7ec06eba43e64fc98bb47eb4defbf5293046aa24
sharonwtff <alejosantos5551@gmail.com>
2026-09-22 16:45:14 +0000

    ⭐S01t0401 for finaliza toma de tiempo de 100 numeros usando un for

--- S02/s01t0401_for.py
+++ S02/s01t0401_for.py
@@ -14,4 +14,16 @@ timestamp_01 = time.time()
 n = 100
 sum= 0
 for number in range(1, n + 1):
- print(str(number) + "  ")
+ sum = sum + number
+ #1: sum <- 0 + 1
+ # #sum =1
+ # #2: sum <-1+2
+ # =3
+ # 3: sum <-3+3
+ # sum =6
+ # 100: sum<- sum_(1)+ 100
+
+print(f"la suma de uno hasta {n} es: {sum}")
+timestamp_02 = time.time()
+print(f"Tiempo de ejecucion: {(timestamp_02 - timestamp_01) *1e6:2f} μs")
+
